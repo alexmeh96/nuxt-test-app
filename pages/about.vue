1c557dd122e30f609d40a156160e20b3866634b6
alexmeh96 <alex.meh96@mail.ru>
2021-01-15 03:01:46 +0300

    nuxt base

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.</p>
+    <p>Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.</p>
+    <p>Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.</p>
+  </section>
+</template>
